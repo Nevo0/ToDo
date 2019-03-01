7b57c0722785ed653fe77a690b3c12ded2847fa0
Nevo0 <Piecyk.rafal@gmail.com>
2019-03-01 21:29:57 +0100

    TodoApp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ToDoInput from "./components/ToDoInput";
+import ToDoList from "./components/ToDoList";
+
+import "./bootstrap.min.css";
+import uuid from "uuid";
 
 class App extends Component {
+  state = {
+    items: [],
+    id: uuid(),
+    item: "",
+    editItem: false
+  };
+
+  handleChange = e => {
+    // console.log(e);
+
+    this.setState({
+      item: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const newItem = {
+      id: this.state.id,
+      item: this.state.item
+    };
+
+    const updatedItems = [...this.state.items, newItem];
+
+    this.setState({
+      items: updatedItems,
+      id: uuid(),
+      item: "",
+      editItem: false
+    });
+  };
+
+  handleClear = () => {
+    this.setState({
+      items: []
+    });
+  };
+
+  hendleDelete = id => {
+    const filteredItems = this.state.items.filter(item => item.id !== id);
+
+    this.setState({
+      items: filteredItems
+    });
+  };
+
+  handleEdit = id => {
+    const filteredItems = this.state.items.filter(item => item.id !== id);
+
+    const selectedItem = this.state.items.find(item => item.id === id);
+
+    this.setState({
+      items: filteredItems,
+      item: selectedItem.item,
+      editItem: !this.state.editItem,
+      id: id
+    });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto col-md-8 mt-4">
+            <h3 className="text-capitalize text-center"> todo input</h3>
+            <ToDoInput
+              item={this.state.item}
+              editItem={this.state.editItem}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+              handleClear={this.handleClear}
+            />
+            <ToDoList
+              items={this.state.items}
+              handleClear={this.handleClear}
+              hendleDelete={this.hendleDelete}
+              handleEdit={this.handleEdit}
+            />
+          </div>
+        </div>
       </div>
     );
   }
